41cd3c13bc74c278e4736e81c53278daac05034e
Yasha <yashasidin333@yandex.ru>
2023-03-20 22:02:56 +0300

    Add not all solves of tasks

---
+++ Task_1/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+void PrintIntTableArray(int[,] tableArray)
+{
+    int rows = tableArray.GetLength(0);
+    int columns = tableArray.GetLength(1);
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            if(j == tableArray.GetLength(1) - 1)
+            {
+                Console.WriteLine($"{tableArray[i, j]}\t");
+                break;
+            }
+            Console.Write($"{tableArray[i, j]}, ");
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------------------
+
+// Заполнить двумерный массив случайными числами
+int[,] CreateAndFillIntTableArray(int countRows, int countColumns, int startRange, int endRange)
+{
+    int[,] tableArray = new int[countRows, countColumns];
+    for(int i = 0; i < countRows; i++)
+    {
+        for(int j = 0; j < countColumns; j++)
+        {
+            tableArray[i, j] = new Random().Next(startRange, endRange + 1);
+        }
+    }
+
+    return tableArray;
+}
+
+// -----------------------------------------------------------------------------------------
+
+// Меняем первую и последнюю строку местами
+int[,] MoveFirstRowToLast(int[,] tableArray)
+{
+    int rows = tableArray.GetLength(0);
+    int columns = tableArray.GetLength(1);
+    for(int j = 0; j < columns; j++)
+    {
+        int temporary = tableArray[0, j];
+        tableArray[0, j] = tableArray[rows - 1, j];
+        tableArray[rows - 1, j] = temporary;
+    }
+    
+    return tableArray;
+}
+
+
+Console.Clear();
+Console.Write("Количество строк массива (m): ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Количество столбцов массива (m): ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] tableArray = CreateAndFillIntTableArray(m, n, 0, 9);
+PrintIntTableArray(tableArray);
+Console.WriteLine();
+MoveFirstRowToLast(tableArray);
+PrintIntTableArray(tableArray);
+
---
+++ Task_2/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 2: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+void PrintIntTableArray(int[,] tableArray)
+{
+    int rows = tableArray.GetLength(0);
+    int columns = tableArray.GetLength(1);
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            if(j == tableArray.GetLength(1) - 1)
+            {
+                Console.WriteLine($"{tableArray[i, j]}\t");
+                break;
+            }
+            Console.Write($"{tableArray[i, j]}, ");
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------------------
+
+// Заполнить двумерный массив случайными числами
+int[,] CreateAndFillIntTableArray(int countRows, int countColumns, int startRange, int endRange)
+{
+    int[,] tableArray = new int[countRows, countColumns];
+    for(int i = 0; i < countRows; i++)
+    {
+        for(int j = 0; j < countColumns; j++)
+        {
+            tableArray[i, j] = new Random().Next(startRange, endRange + 1);
+        }
+    }
+
+    return tableArray;
+}
+
+// -----------------------------------------------------------------------------------------
+
+// Транспонируем
+int[,] Transponation(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    int shift = 0;
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = shift; j < columns; j++)
+        {
+            int temporary = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temporary;
+        }
+        shift++;
+    }
+
+    return matrix;
+}
+
+Console.Clear();
+Console.Write("Количество строк массива (m): ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Количество столбцов массива (m): ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] tableArray = CreateAndFillIntTableArray(m, n, 0, 9);
+PrintIntTableArray(tableArray);
+Console.WriteLine();
+Transponation(tableArray);
+PrintIntTableArray(tableArray);
+
